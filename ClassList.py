ab7e8172e9f22a1702407188930e7f924d0a4edd
piko322 <dhilansunandabong@gmail.com>
2023-08-31 12:02:28 +0700

    WIP: webregParser.py and ClassList.py

---
+++ ClassList.py
@@ -0,0 +1,9 @@
+"""
+ClassList
+~~~~~~~~~
+
+A library for creating a linked list representing a weekly schedule using the CourseNode class.
+Has functions to read from a pdf file, parse it using the webregParser library, and add the courses to the linked list.
+"""
+import webregParser
+import CourseNode
